91585c944395fe49a5de5d91c8fe060b5be9415a
ninochakvetadze <nino.chakvetadze28b@geolab.edu.ge>
2023-08-04 23:50:58 -0700

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,26 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: "standard",
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: [".eslintrc.{js,cjs}"],
-            parserOptions: {
-                sourceType: "script",
-            },
-        },
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    rules: {
-        "no-func-assign": "off",
-        "no-console": "off",
-        "no-alert": "off",
-        "no-underscore-dangle": "off",
-        "no-empty": "off",
-        "no-magic-numbers": "off",
-        "no-extra-semi": "off",
-        "no-restricted-globals": "off",
-        "no-shadow": "off",
-        camelcase: "off",
-    },
-};
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
